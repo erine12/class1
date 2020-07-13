e63750bb0bd598cfa808da7aa5f6ea9396cc19ec
erine <egueppi@yahoo.fr>
2020-07-13 01:41:01 -0400

    first commit

---
+++ FirstJavaProgram.java
@@ -0,0 +1,5 @@
+public class FirstJavaProgram {
+    public static void main(String[] args){
+      System.out.println("this is my first java program");
+    }//End of main
+  }//End of FirstJavaProgram Class
